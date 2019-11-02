cff6a21c646210474179b55be88bfc63112f841d
Corinne Fair <corinne.fair.17@cnu.edu>
2019-11-02 17:41:15 -0400

    minor formatting & commented out test stuff

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -13,18 +13,17 @@
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace KP_StringParserClass;
-//TODO Fill in
 
 //dont forget to initialize member variables
 StringParserClass::StringParserClass(void) {
 	pStartTag = nullptr;
-	pEndTag   = nullptr;
+	pEndTag = nullptr;
 	areTagsSet = false;
 }
 
 //call cleanup to release any allocated memory
 StringParserClass::~StringParserClass(void) {
-	// cleanup();
+	cleanup();
 }
 
 //these are the start tag and the end tags that we want to find,
@@ -40,10 +39,10 @@ int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 	}
 
 	int startLen = strlen(pStart);
-	int endLen   = strlen(pEnd);
+	int endLen = strlen(pEnd);
 
-	pStartTag = new char[startLen+1];
-	pEndTag   = new char[endLen+1];
+	pStartTag = new char[startLen + 1];
+	pEndTag = new char[endLen + 1];
 
 	strncpy(pStartTag, pStart, startLen);
 	strncpy(pEndTag, pEnd, endLen);
@@ -58,13 +57,14 @@ int StringParserClass::setTags(const char *pStart, const char *pEnd) {
 //SUCCESS  finished searching for data between tags, results in myVector (0 or more entries)
 //ERROR_TAGS_NULL if either pStart or pEnd is null
 //ERROR_DATA_NULL pDataToSearchThru is null
-int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector) {
-	if (pDataToSearchThru == nullptr) {
-		return ERROR_DATA_NULL;
-	}
-
+int StringParserClass::getDataBetweenTags(char *pDataToSearchThru,
+		std::vector<std::string> &myVector) {
 	if (pStartTag == nullptr || pEndTag == nullptr) {
 		return ERROR_TAGS_NULL;
+	}
+
+	if (pDataToSearchThru == nullptr) {
+		return ERROR_DATA_NULL;
 	}
 
 	myVector.clear();
@@ -73,17 +73,20 @@ int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<s
 	int tagLen = strlen(pStartTag);
 	bool noTagFound = false;
 	size_t foundStart = str.find(pStartTag);
-	size_t foundEnd   = str.find(pEndTag);
+	size_t foundEnd = str.find(pEndTag);
 
+	// std::cout << "got here" << std::endl;
 	while (!noTagFound) {
-		if(foundStart != std::string::npos && foundEnd != std::string::npos) {
-			std::string tempStr = str.substr(foundStart+tagLen, foundEnd-tagLen-foundStart);
+		if (foundStart != std::string::npos && foundEnd != std::string::npos) {
+			std::string tempStr = str.substr(foundStart + tagLen,
+					foundEnd - tagLen - foundStart);
+			// std::cout << tempStr << std::endl;
 			myVector.push_back(tempStr);
-			foundStart = str.find(pStartTag, foundStart+1);
-			foundEnd   = str.find(pEndTag, foundEnd+1);
-		}
-		else {
+			foundStart = str.find(pStartTag, foundStart + 1);
+			foundEnd = str.find(pEndTag, foundEnd + 1);
+		} else {
 			noTagFound = true;
+			// std::cout << "no tags found" << std::endl;
 		}
 	}
 
@@ -91,7 +94,13 @@ int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<s
 }
 
 void StringParserClass::cleanup() {
-
+	if (pStartTag) {
+		delete[] pStartTag;
+	}
+	if (pEndTag) {
+		delete[] pEndTag;
+	}
+	areTagsSet = false;
 }
 
 //Searches a string starting at pStart for pTagToLookFor
@@ -99,7 +108,22 @@ void StringParserClass::cleanup() {
 //SUCCESS  found pTagToLookFor, pStart points to beginning of tag and pEnd points to end of tag
 //FAIL did not find pTagToLookFor and pEnd points to 0
 //ERROR_TAGS_NULL if either pStart or pEnd is null
-int StringParserClass::findTag(char *pTagToLookFor, char *&pStart, char *&pEnd) {
+int StringParserClass::findTag(char *pTagToLookFor, char *&pStart,
+		char *&pEnd) {
+	if (pStart == nullptr || pEnd == nullptr) {
+		return ERROR_TAGS_NULL;
+	}
 
-	return -100;
+	std::string str = pStart;
+
+	size_t found = str.find(pTagToLookFor);
+
+	if (found == std::string::npos) {
+		return FAIL;
+	}
+
+	*pStart = found;
+	*pEnd = found + strlen(pTagToLookFor);
+
+	return SUCCESS;
 }
